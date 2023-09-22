19522fcd2e06e2993008a80c429856f8111f5494
rujack <76602103+rujack@users.noreply.github.com>
2023-09-22 17:11:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ const port = process.env.PORT || 3000;
 
 // Middleware untuk mengizinkan CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 // Endpoint proxy untuk memanggil API 1inch.dev
 app.get('/quote', async (req, res) => {
-  try {
-    const apiUrl = 'https://api.1inch.dev/swap/v5.2/56/quote';
-    const params = {
-      src: req.query.src,
-      dst: req.query.dst,
-      amount: req.query.amount,
-    };
-    const token = req.header('Authorization');
-    const headers = {
-        Authorization: token,
-      };
+    try {
+        const apiUrl = 'https://api.1inch.dev/swap/v5.2/56/quote';
+        const params = {
+            src: req.query.src,
+            dst: req.query.dst,
+            amount: req.query.amount,
+        };
+        const token = req.header('Authorization');
+        const headers = {
+            Authorization: token,
+        };
 
-    const response = await axios.get(apiUrl, { params,headers });
+        const response = await axios.get(apiUrl, { params, headers });
 
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    if (error.response.status == 400) {
+        res.json(response.data);
+    } catch (error) {
+        // console.error(error);
+        if (error.response.status == 400) {
             res.status(error.response.status).json(error.response.data);
         } else if (error.response.status == 429) {
             res.status(error.response.status).json({
@@ -41,19 +41,20 @@ app.get('/quote', async (req, res) => {
         else {
             res.status(500).json({ error: 'Internal Server Error' });
         }
-  }
+    }
 });
 
-app.get('/',(req,res)=>{
-    res.status(200).json('proxy nih boss!! awokawok')
+app.get('/', (req, res) => {
+    res.status(200).send('proxy nih boss!!')
 })
 
-// app.get('/*', (req, res) => {
-//     res.status(404).send('NOT FOUND NIH BOSS!!')
-// })
+app.get('/*', (req, res) => {
+    res.status(404).send('NOT FOUND NIH BOSS!!')
+})
 
+// Jalankan server
 app.listen(port, () => {
-  console.log(`Proxy server is running on port ${port}`);
+    console.log(`Proxy server is running on port ${port}`);
 });
 
 module.exports = app
